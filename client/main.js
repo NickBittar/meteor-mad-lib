9bc9cead9880bd3e5e6e5e09da9c11be51611782
NickBittar <Nickcb7@gmail.com>
2016-04-04 20:56:51 -0400

    Added event listener to submit button to log 'Hello, world' into the console.

--- client/main.js
+++ client/main.js
@@ -3,20 +3,18 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.madLib.onCreated(function madLibOnCreated() {
+  
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.madLib.helpers({
+
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.madLib.events({
+  'click #submit-button'(event, instance) {
+    event.preventDefault();
+
+    console.log("Hello, world");
   },
 });
