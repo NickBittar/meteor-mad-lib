1ceea493469bf7a8823a210a3aadeff6494c9593
Rainer Paskiewicz <rainer.paskiewicz@gmail.com>
2016-04-04 21:17:21 -0400

    Created method to post form to console

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.madLib.onCreated(function madLibOnCreated() {
-  
+
 });
 
 Template.madLib.helpers({
@@ -13,8 +13,14 @@ Template.madLib.helpers({
 
 Template.madLib.events({
   'click #submit-button'(event, instance) {
-    event.preventDefault();
+    // event.preventDefault();
 
     console.log("Hello, world");
+
   },
+  'submit #form'(event, instance){
+    event.preventDefault();
+    let form = event.target;
+    console.info("'" +form.exclamation.value + "!', you say, as you " + form.adverb.value + " jump onto your " + form.noun.value + " and " + form.verb.value +" into the sunset.");
+  }
 });
